69e13d18bfc234ba263e45a55f678098481c193d
Jang184 <e2joo418@gmail.com>
2021-08-09 14:59:18 +0900

    Edit : 장바구니 view 수정

--- orders/views.py
+++ orders/views.py
@@ -4,8 +4,8 @@ from django.http  import JsonResponse
 from django.views import View
 
 from users.models	   import User
-from products.models import Product
-from orders.models   import Cart
+from products.models   import Product
+from orders.models     import Cart
 from users.utils 	   import LoginDecorator 
 
 class CartView(View):
@@ -18,36 +18,26 @@ class CartView(View):
 
 		product = Product.objects.get(id=data['product_id'])
 
-		if Cart.objects.filter(product=product).exists():
-			cart = Cart.objects.get(product=product)
+		cart, flag = Cart.objects.get_or_create(product=product, user=user, quantity=data['quantity'])
+		if not flag:
 			cart.quantity += data['quantity']
 			cart.save()
 			return JsonResponse({'message':'ADD_SUCCESS'},status=200)
-		
-		Cart.objects.create( 
-			user_id    = user.id,
-			product_id = product.id,
-			quantity   = data['quantity']
-			)
 		return JsonResponse({'message' : 'SUCCESS'}, status=200)
 
 	@LoginDecorator
 	def get(self, request):
-		user = request.user
-		results = []
+		user  = request.user
 		carts = Cart.objects.filter(user=user)
 
 		if not carts.exists():
-			return JsonResponse({'results' : []}, status=200)
-
-		for cart in carts:
-			product = Product.objects.get(id=cart.product_id)
-			results.append({
-				"product_name" : product.name,
-				"price" : round(product.price),
-				"discount" : round(int(product.price) * 0.9) ,
-				"quantity" : cart.quantity
-				})
+			return JsonResponse({'message' : 'NO_CART'}, status=200)
+		
+		results=[{
+		"name" 	   : cart.product.name,
+		"price"    : round(cart.product.price),
+		"discount" : round(int(cart.product.price) * 0.9) ,
+		"quantity" : cart.quantity} for cart in carts]
 
 		return JsonResponse({'results': results}, status=200)
 					
