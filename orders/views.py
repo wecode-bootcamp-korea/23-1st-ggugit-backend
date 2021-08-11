391dd2f241e602bb24320896dd30870afe6f8369
Jang184 <e2joo418@gmail.com>
2021-08-11 18:08:03 +0900

    장바구니 patch 추가 post 재고부족(no stock) 메세지 추가

--- orders/views.py
+++ orders/views.py
@@ -10,18 +10,25 @@ from .models      import User, Product, Cart
 class CartView(View):
 	@LoginDecorator
 	def post(self,request):
-		data = json.loads(request.body)
-		user = request.user
+		data     = json.loads(request.body)
+		user     = request.user
+		quantity = data['quantity']
+
 		if not Product.objects.filter(id=data['product_id']).exists():
 			return JsonResponse({'message' : 'NOT_FOUND'}, status=400)
 
 		product = Product.objects.get(id=data['product_id'])
 
-		cart, created = Cart.objects.get_or_create(product=product, user=user, quantity=data['quantity'])
+		if product.stock < quantity:
+			return JsonResponse({'message':'no_stock'}, status=400)
+
+		cart, created = Cart.objects.get_or_create(product=product, user=user, defaults = {'quantity' : quantity})
 		if not created:
-		    cart.quantity += data['quantity']
-		    cart.save()
-		    return JsonResponse({'message':'ADD_SUCCESS'},status=200)
+			if product.stock < quantity + cart.quantity:
+				return JsonResponse({'message':'no_stock'}, status=400)
+			cart.quantity += data['quantity']
+			cart.save()
+			return JsonResponse({'message':'ADD_SUCCESS'},status=200)
 		return JsonResponse({'message' : 'SUCCESS'}, status=200)
 
 	@LoginDecorator
@@ -30,7 +37,7 @@ class CartView(View):
 		carts = Cart.objects.filter(user=user)
 
 		if not carts.exists():
-			return JsonResponse({'message' : 'NO_CART'}, status=200)
+			return JsonResponse({'message' : 'NO_CART'}, status=400)
 		
 		results=[{
 		"name"     : cart.product.name,
@@ -40,14 +47,28 @@ class CartView(View):
 
 		return JsonResponse({'results': results}, status=200)
 
-    @LoginDecorator
-    def delete(self,request,cart_id):
+	@LoginDecorator
+	def patch(self,request,cart_id):
+		data   = json.loads(request.body)
+		user   = request.user
+		stocks = Product.objects.get(id = data['product_id']).stock
+		cart   = Cart.objects.get(id=cart_id)
+		if stocks < cart.quantity + data['quantity']:
+			return JsonResponse({'message':'OUT_OF_STOCK'},status = 400)
+		if Cart.objects.get(id=cart_id).user_id != user.id:
+			return JsonResponse({'message':'INVALED_USER'}, status=403)
+		cart.quantity += data['quantity']
+		cart.save()
+		return JsonResponse({'message':'SUCCESS'}, status = 201)
+
+	@LoginDecorator
+	def delete(self,request,cart_id):
         
-        if not Cart.objects.filter(id = cart_id).exists():
-            return JsonResponse({'message': 'NOT_FOUND'}, status = 404)
+		if not Cart.objects.filter(id = cart_id).exists():
+			return JsonResponse({'message': 'NOT_FOUND'}, status = 404)
 
-        if Cart.objects.get(id=cart_id).user_id != request.user.id:
-            return JsonResponse({'message':'INVALED_USER'}, status=403)
+		if Cart.objects.get(id=cart_id).user_id != request.user.id:
+			return JsonResponse({'message':'INVALED_USER'}, status=403)
 
-        if Cart.objects.get(id = cart_id).delete():
-            return JsonResponse({'message': 'SUCCESS'}, status = 200)        
+		if Cart.objects.get(id = cart_id).delete():
+			return JsonResponse({'message': 'SUCCESS'}, status = 200)        
