309604b835342feb96a9649748adbed06f842402
김훈태 <hoontae.kim@Hoontaes-MacBookPro.local>
2021-08-12 11:39:30 +0900

    Add: 메인이미지 배너2

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ from products.views import MainView, ProductView, ProductDetailView
 urlpatterns = [
     path('/main', MainView.as_view()),
     path('', ProductView.as_view()),
-    path('/<int:product_id>', ProductDetailView.as_view())
+    path('/<int:product_id>', ProductDetailView.as_view()),
 ]
